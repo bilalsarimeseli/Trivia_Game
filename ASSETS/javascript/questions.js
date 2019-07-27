329a6ee553b87c1d3fb7f9206c75ae65643bc65a
Bilal Sarımeşeli <bilalsarimeseli@Bilals-MacBook-Air.local>
2019-07-27 06:33:36 -0700

    File error problem is fixed and game is oprational

---
+++ ASSETS/javascript/questions.js
@@ -0,0 +1,46 @@
+const quizQuestions = [
+    {
+        question: "What sign do we use to grab an element with jQuery?",
+        choices: ["$", "!", "@", "*"],
+        correctAnswer: "$"
+    },                                  
+
+    {
+        question: "What scripting language is jQuery written in?",
+        choices: ["C#", "JavaScript", "C++", "Python"],
+        correctAnswer: "JavaScript"
+    },
+
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["javascript", "js", "jscript", "jsc"],
+        correctAnswer: "js"
+    },
+    
+    {
+        question: "How do you create a function in JavaScript?",
+        choices: ["function = myFunction()", "funct = myFunction()", "function : myFunction();", "f = myFunction()"],
+        correctAnswer: "function = myFunction()"
+    }, 
+
+    {
+        question: "How do you round the number 7.25, to the nearest integer?",
+        choices: ["rnd(7.25)", "round(7.25)", "Math.floor(7.25)", "Math.round(7.25)"],
+        correctAnswer: "Math.floor(7.25)"
+    },
+
+];
+
+const funImages = [
+    './ASSETS/images/happy fun.gif',
+    './ASSETS/images/hellYeah.gif',
+    './ASSETS/images/winning.gif',
+    './ASSETS/images/yeah.gif',
+    './ASSETS/images/you did it.gif'
+];
+
+const sadImages = [
+    './ASSETS/images/danm.gif',
+    './ASSETS/images/oops.gif',
+    './ASSETS/images/pretty bad.gif'
+];
